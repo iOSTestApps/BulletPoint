d15a93450bccfad96a7362600f2f7b180b8c8cb9
Christopher Stott <christopherstott@gmail.com>
2012-07-01 18:20:55 +0800

    Initial Commit

---
+++ BulletPoint/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BulletPoint
+//
+//  Created by Chris Stott on 12-07-01.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BulletPoint/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BulletPoint
+//
+//  Created by Chris Stott on 12-07-01.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
