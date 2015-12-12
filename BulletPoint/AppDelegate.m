2be108508b8705257891f4f0c1496209e9d81ff3
Christopher Stott <christopherstott@gmail.com>
2015-12-11 18:01:17 -0800

    Remove the default test appearance

--- BulletPoint/AppDelegate.m
+++ BulletPoint/AppDelegate.m
@@ -25,7 +25,7 @@ AppDelegate* _instance = 0;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    [[UITextView appearance] setTextColor:[UIColor whiteColor]];
+//    [[UITextView appearance] setTextColor:[UIColor whiteColor]];
     [BuddyBuildSDK setup];
     
 	_instance = self;
